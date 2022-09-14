3bbedbdc3738b07d6afc5691eec78eebf79cbaa4
Ritik <78131705+srivastavaritik@users.noreply.github.com>
2022-09-14 12:46:41 +0530

    ______25

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  const code =
-    `Welcome to text-share! 
+  const code = `Welcome to text-share! 
 
 # Use the commands in the top right corner
   to create a new file to share with others`;
   res.render("code-display", { code });
+});
+
+app.get("/new", (req, res) => {
+  res.render("new");
 });
 
 app.listen(3000);
