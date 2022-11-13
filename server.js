95cd17c40b9ed87fa42f7930ea6b9589c84b9f31
Ritik Kumar <srivastavaritik360@gmail.com>
2022-11-13 12:45:17 +0530

    completed

--- server.js
+++ server.js
@@ -3,17 +3,50 @@ const res = require("express/lib/response");
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true}))
+
+const Document = require("./models/Document")
+const mongoose = require("mongoose")
+mongoose.connect(
+  "mongodb+srv://admin:8480pass@codebin.gk99noa.mongodb.net",
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  }
+);
 
 app.get("/", (req, res) => {
   const code = `Welcome to text-share! 
 
 # Use the commands in the top right corner
   to create a new file to share with others`;
-  res.render("code-display", { code });
+  res.render("code-display", { code, language: 'plaintext' });
 });
 
 app.get("/new", (req, res) => {
   res.render("new");
 });
+
+app.post('/save', async(req,res) => {
+  const value = req.body.value
+  try {
+    const document = await Document.create({value})
+    res.redirect(`/${document.id}`)
+  } catch (error) {
+    res.render('new', {value})
+  }
+  console.log(value)
+})
+
+app.get('/:id', async(req, res) => {
+  const id = req.params.id
+  try {
+    const document = await Document.findById(id)
+
+    res.render('code-display',{code:  document.value})
+  } catch (error) {
+    res.redirect('/')
+  }
+})
 
 app.listen(3000);
